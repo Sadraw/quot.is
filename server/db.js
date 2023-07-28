459c20e182330a2637ae75604de52364972fdbbc
sadra <sadrathescientist@gmail.com>
2023-07-28 19:25:50 +0200

    Modify db.js

--- server/db.js
+++ server/db.js
@@ -1,4 +1,3 @@
-// db.js
 require("dotenv").config({ path: "../mysql.env" });
 const mysql = require("mysql");
 
@@ -7,6 +6,20 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+});
+
+// Log the connection configuration
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_DATABASE:", process.env.DB_DATABASE);
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+  } else {
+    console.log("Connected to MySQL database!");
+  }
 });
 
 module.exports = connection;
