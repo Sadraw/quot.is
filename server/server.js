fc7213261282fd8dd4f7bf906f15cb373f3438df
sadra <sadrathescientist@gmail.com>
2023-08-07 13:51:00 +0200

    Modify Port on Server.js

--- server/server.js
+++ server/server.js
@@ -186,12 +186,8 @@ const credentials = { key: privateKey, cert: certificate };
 const httpsServer = https.createServer(credentials, app);
 
 // Start the server on the default HTTPS port (443)
-httpsServer.listen(5000, "127.0.0.1", () => {
-<<<<<<< HEAD
-  console.log("Server is doing something on https://api.quot.is/");
-=======
+httpsServer.listen(5001, "127.0.0.1", () => {
   console.log("Server is doing something on https://api.quot.is:5000");
->>>>>>> 82da4ff2807fe30eb584dce833eed25c2e312830
 });
 
 
