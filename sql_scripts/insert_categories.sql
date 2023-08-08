bd5e32d02d825e756cb352f43bc5778ad30e1820
sadra <sadrathescientist@gmail.com>
2023-08-08 23:33:35 +0200

    Add sql_scripts folder with a few examples

---
+++ sql_scripts/insert_categories.sql
@@ -0,0 +1,11 @@
+INSERT INTO categories (name) VALUES
+  ('happiness'),
+  ('motivation'),
+  ('creativity'),
+  ('success'),
+  ('friendship'),
+  ('positivity'),
+  ('life'),
+  ('change'),
+  ('education'),
+  ('courage');
