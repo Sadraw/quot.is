1f7acfd7fdac0ba5b8f689256dea4cb77bce537d
sadra <sadrathescientist@gmail.com>
2023-08-08 22:39:01 +0200

    Delete console.log in server.js

--- server/server.js
+++ server/server.js
@@ -121,7 +121,6 @@ const httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(5000, "127.0.0.1", () => {
   console.log("Server is doing something on https://api.quot.is");
-  console.log(db);
 });
 
 async function fetchSentQuoteIds(clientId) {
