57f51868bb937f19535c1a8d8d85341cd84ed5f8
sadra <sadrathescientist@gmail.com>
2023-08-10 23:47:57 +0200

    init Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/common/Header';
 
 function App() {
   return (
     <div>
-          <img
-            src="https://socialify.git.ci/Sadraw/quot.is/image?font=Jost&forks=1&issues=1&language=1&name=1&owner=1&pulls=1&stargazers=1&theme=Dark"
-            alt="quot.is"
-            width="640"
-            height="320"
-          />
+    
+    
+    
+
+    
+    <Header />
+
+
+
+
+
     </div>
   );
 }
---
+++ client/src/components/common/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../../Styles/header.css'
+
+const Header = () => {
+
+    return(
+
+        <div className="header">
+            <h1><span className="typing-text">Quot.is</span></h1>
+            
+            
+        </div>
+    )
+
+}
+
+export default Header;
