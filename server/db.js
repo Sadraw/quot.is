1564bca0fa89ebe82c6cc31fa339705c6b83c8da
sadra <sadrathescientist@gmail.com>
2023-08-16 15:31:05 +0200

    Modify db.js for gracefulShutdown function

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 require("dotenv").config({ path: "../mysql.env" });
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise"); // Use the promise version of mysql2 library
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  connectionLimit: 10,
 });
 
 // Log the connection configuration
@@ -14,12 +15,16 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("DB_DATABASE:", process.env.DB_DATABASE);
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err.message);
-  } else {
-    console.log("Connected to MySQL database!");
-  }
-});
+// Graceful Shutdown
+const gracefulShutdown = () => {
+  console.log("Database connections closing gracefully...");
+  pool.end().then(() => {
+    console.log("Database connections closed.");
+    process.exit(0); // Exit with a success status
+  });
+};
 
-module.exports = connection;
+process.on("SIGINT", gracefulShutdown); // Handle Ctrl+C
+process.on("SIGTERM", gracefulShutdown); // Handle termination signal
+
+module.exports = pool;
