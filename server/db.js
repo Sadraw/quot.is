5033dff041c7ab124f4cd08caf2e09953d3f69b2
sadra <sadrathescientist@gmail.com>
2023-08-16 15:44:31 +0200

    Modify db.js without connection limist

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
 require("dotenv").config({ path: "../mysql.env" });
-const mysql = require("mysql2/promise"); // Use the promise version of mysql2 library
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  connectionLimit: 10,
 });
 
 // Log the connection configuration
@@ -15,19 +14,12 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("DB_DATABASE:", process.env.DB_DATABASE);
 
-// Graceful Shutdown
-const gracefulShutdown = () => {
-  console.log("Database connections closing gracefully...");
-  pool.end().then(() => {
-    console.log("Database connections closed.");
-    process.exit(0); // Exit with a success status
-    // Perform any cleanup tasks you want, such as logging or saving state.
-    // You can also close any resources that need to be explicitly closed.
-    // You don't have to close the database connections here.
-  });
-};
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+  } else {
+    console.log("Connected to MySQL database!");
+  }
+});
 
-process.on("SIGINT", gracefulShutdown); // Handle Ctrl+C
-process.on("SIGTERM", gracefulShutdown); // Handle termination signal
-
-module.exports = pool;
+module.exports = connection;
