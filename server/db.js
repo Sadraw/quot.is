82c47c679581ed0a2308d186b72c3b25a7d26527
sadra <sadrathescientist@gmail.com>
2023-08-17 18:58:49 +0200

    Use pool instead of direct connection on db.js

--- server/db.js
+++ server/db.js
@@ -6,6 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  connectionLimit: 10, // Adjust as needed
 });
 
 // Log the connection configuration
@@ -14,12 +15,29 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("DB_DATABASE:", process.env.DB_DATABASE);
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err.message);
-  } else {
-    console.log("Connected to MySQL database!");
-  }
+// Log when a new connection is acquired from the pool
+pool.on("acquire", (connection) => {
+  console.log("Connection %d acquired", connection.threadId);
 });
 
-module.exports = connection;
+// Log when a connection is released back to the pool
+pool.on("release", (connection) => {
+  console.log("Connection %d released", connection.threadId);
+});
+
+// Log if an error occurs in the connection pool
+pool.on("error", (err) => {
+  console.error("Connection pool error:", err);
+});
+
+// Export the pool instead of the connection
+module.exports = pool;
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to the database:", err.message);
+//   } else {
+//     console.log("Connected to MySQL database!");
+//   }
+// });
+// module.exports = connection;
