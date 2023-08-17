6e4c3500b23a8825571b5d436b5b7f00d65f8ff1
sadra <sadrathescientist@gmail.com>
2023-08-17 19:07:30 +0200

    Modify db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,17 @@
 require("dotenv").config({ path: "../mysql.env" });
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+// ... connection
+
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   connectionLimit: 10, // Adjust as needed
+// });
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
