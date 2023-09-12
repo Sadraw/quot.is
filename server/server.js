3f48eb5fdfe875bc498d553fb535271e1ba44141
sadra <sadrathescientist@gmail.com>
2023-09-12 22:44:47 +0200

    Add fetchAuthorName

--- server/server.js
+++ server/server.js
@@ -64,6 +64,27 @@ async function fetchSentQuoteIds(clientId) {
     });
   });
 }
+async function fetchAuthorName(authorId) {
+  return new Promise((resolve, reject) => {
+    const sql = `
+      SELECT q.text AS quote, a.name AS author
+      FROM quotes AS q
+      JOIN authors AS a ON q.authorId = a.id
+      WHERE q.id = ?;
+    `;
+    dbPool.query(sql, [authorId], (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (result.length > 0) {
+          resolve(result[0].author); // Get the author's name from the query result
+        } else {
+          reject(new Error("Author not found"));
+        }
+      }
+    });
+  });
+}
 
 async function fetchUnsentQuotes(sentQuoteIds, categoryIds) {
   return new Promise((resolve, reject) => {
