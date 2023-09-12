40d2ddaeac42d8ae8385132b33ff8455be3a4306
sadra <sadrathescientist@gmail.com>
2023-09-12 23:51:26 +0200

    Use the author name from the selected quote

--- server/server.js
+++ server/server.js
@@ -156,11 +156,9 @@ app.get("/v1/quote", async (req, res) => {
     const selectedQuote = getRandomQuote(unsentQuotes);
 
     const authorId = selectedQuote.authorId;
-    // const authorName = await fetchAuthorName(authorId);
-    const authorName = selectedQuote.author; // Use the author name from the selected quote
-    
-    const categoryIdsForSelectedQuote = [selectedQuote.categoryId]; // Fetch category for selected quote only
-    const categoryNames = await fetchCategoryNames(categoryIdsForSelectedQuote);
+    const authorName = await fetchAuthorName(authorId);
+
+    const categoryNames = unsentQuotes.map((quote) => quote.categoryName);
 
     console.log("Sending response...");
     res.json({
