830cfa7457e55d3ea0ed933e99c61625b22ab305
sadra <sadrathescientist@gmail.com>
2023-09-17 21:53:08 +0200

    Add Components

---
+++ client/quot/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./styles/App.css";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/quot/src/components/HeroSection.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import '../styles/HeroSection.css'
+
+
+const HeroSection = () => {
+
+    return(
+
+        <section className="hero"> 
+        <div className="hero-content">
+
+            
+            <h1>Welcome to quot.is</h1>
+            <p>Your Source for Inspiring Quotes and captivating Facts </p>
+            <button className="cta-button">Get Started</button>
+            
+            
+            </div>
+            </section>
+        
+    )
+}
+
+export default HeroSection;
