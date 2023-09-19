d59e823a4cc6ad598cda5cad49a779ae6c1c4809
sadra <sadrathescientist@gmail.com>
2023-09-19 21:27:41 +0200

    Modify Header with correct links

--- client/quot/src/components/Header.tsx
+++ client/quot/src/components/Header.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import '../styles/Header.css'
 const Header = () => {
 
+
+
     return(
 
         <header>
             <nav className="nav-menu">
                 <ul>
-                    <li> <a href="#Home">Home</a></li>
+                    <li> <a href="/">Home</a></li>
                     <li> <a href="#blog">Blog</a></li>
                     <li> <a href="#Team">Team</a></li>
                     <li> <a href="#about">About</a></li>
