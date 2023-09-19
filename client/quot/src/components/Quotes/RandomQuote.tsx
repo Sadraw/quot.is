456a94943f4718319863cbbb88aeb0c1079d958c
sadra <sadrathescientist@gmail.com>
2023-09-19 21:28:24 +0200

    init Randomquote

--- client/quot/src/components/Quotes/RandomQuote.tsx
+++ client/quot/src/components/Quotes/RandomQuote.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import apiKey from "./api-key";
+import "../../styles/RandomQuote.css"
 
 
 
@@ -7,15 +8,54 @@ const RandomQuote = () => {
     const [quote, setQuote] = useState("");
     const [author, setAuthor] = useState("");
     const [imageUrl, setImageUrl] = useState("");
+    const [categories, setCategories] = useState("");
 
     useEffect(() => {
+        const apiUrl = "https://api.quot.is/v1/quote";
+        
         // making the api request to the server 
-        fetch("https://api.quot.is/v1/quote");
-    })
-    return (
-        <div>
+    
+        fetch(apiUrl, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
 
+        })
+        .then((response) => response.json())
+        .then((data: {quote: string; author: string; imageUrl: string; categories: string;}) => {
+            // update the state with the api response data
+            setQuote(data.quote);
+            setAuthor(data.author);
+            setImageUrl(data.imageUrl);
+            setCategories(data.categories);
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+
+        });
+
+    })
+    
+    return (
+
+        <section className="RandomQuote">
+
+        <div className="RandomQuote-content">
+                
+            <h1 >Random Quote of the Day</h1>
+
+            <img className="Image" src={imageUrl} alt={author} />
+            
+            <p>{quote}</p>
+
+            <p>{author}</p>
+
+            <p>{categories}</p>
+            
         </div>
+
+        </section>
+
     )
 }
 
