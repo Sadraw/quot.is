72f26be703b6d8926e56a21f8a97ceee2bf2b41a
sadra <sadrathescientist@gmail.com>
2023-09-19 21:25:22 +0200

    Modify App.tsx to use Route

--- client/quot/src/App.tsx
+++ client/quot/src/App.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./styles/App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import RandomQuote from "./components/Quotes/RandomQuote";
+
 function App() {
   return (
-    <div>
-      <Header />
-      <HeroSection />
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+
+        <Routes>
+          <Route path="/" element= {<HeroSection />} />  
+          <Route path="/quot/src/components/Quotes/RandomQuote.tsx" element={<RandomQuote />} />
+        </Routes>    
+        
+      </div>
+    </Router>
+
+
   );
 }
 
